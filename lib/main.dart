01ffb22fe42ffc09b00229d6c9f27b6791ecd5f9
leloms472-source <leloms472@gmail.com>
2026-06-10 01:25:30 -0300

    Primer inicio de LELOMS

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,38 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const LelomsApp());
+}
+
+class LelomsApp extends StatelessWidget {
+  const LelomsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'LELOMS',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          '🌙 LELOMS',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
